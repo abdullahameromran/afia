d66627c6bda736c8b43d977afb641f3a013e8c62
Abdullah Amer <amer@gensystem.net>
2025-06-08 12:56:10 +0000

    add this clarity script:<script type="text/javascript">     (function(c,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
 import { AuthProvider } from '@/context/AuthContext';
+import Script from 'next/script'; // Import the Script component
 
 export const metadata: Metadata = {
   title: 'Afia Balsemak - صحتكِ تهمنا',
@@ -27,6 +28,15 @@ export default function RootLayout({
           {children}
         </AuthProvider>
         <Toaster />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "rwabuhk9fl");
+          `}
+        </Script>
       </body>
     </html>
   );
